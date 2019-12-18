74be2caa25a48366f27c0437a1d5267d7d86deb2
GerardJoling <wild1702@student.nhl.nl>
2019-12-18 21:16:44 +0100

    1st
    1str

---
+++ Assets/3DVoxel_Free/FantasyPack02/readme_en.txt
@@ -0,0 +1,44 @@
+/*------------------------------------------------------------------------------------*/
+3DVoxel_FantasyPack 02 Free
+made by Mochimagro
+ver. 1.0.0
+/*------------------------------------------------------------------------------------*/
+"3DVoxel_FantasyPack 02 Free" is a 3D Voxel Character Models Asset.
+
+-Models-
+
+- Fighter
+3,188 polygons
+
+- Healer
+3,416 polygons
+
+- SengokuOtome
+4,008 polygons
+
+- Thief
+3,132 polygons
+
+- Common Point
+File Type : .fbx
+Texture Size : 512 * 512
+Rig Type : Humanoid
+
+
+-Contact-
+
+Email - jr.mochimagro@gmail.com
+
+
+-How to Use-
+
+Select the character model you want to use from [3DVoxel_Free > FantasyPack02 > Prefabs],
+and place it in the Scene View.
+Since the model has already adapted Humanoid Rig,
+it can be used immediately by ataching Animator for Humanoid etc.
+
+
+-Version History-
+
+1.0.0
+- First version
---
+++ Assets/Scripts/Photon/GameControllers/AvatarCombat.cs
@@ -0,0 +1,60 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+
+public class AvatarCombat : MonoBehaviour
+{
+
+    private PhotonView PV;
+    private AvatarSetup avatarSetup;
+    public Transform rayOrigin;
+
+    public Text healthDisplay;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PV = GetComponent<PhotonView>();
+        avatarSetup = GetComponent<AvatarSetup>();
+        healthDisplay = GameSetup.GS.healthDisplay;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!PV.IsMine)
+        {
+            return;
+        }
+        if (Input.GetMouseButton(0))
+        {
+            PV.RPC("RPC_Shooting", RpcTarget.All);
+        }
+        healthDisplay.text = avatarSetup.playerHealth.ToString();
+    }
+
+    [PunRPC]
+    void RPC_Shooting()
+    {
+        RaycastHit hit;
+            if (Physics.Raycast(rayOrigin.position, rayOrigin.TransformDirection(Vector3.forward), out hit, 1000))
+            {
+                Debug.DrawRay(rayOrigin.position, rayOrigin.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+                Debug.Log("hit");
+                if(hit.transform.tag == "Avatar")
+                {
+                    hit.transform.gameObject.GetComponent<AvatarSetup>().playerHealth -= avatarSetup.playerDamage;
+                }
+            }
+            else
+            {
+                Debug.DrawRay(rayOrigin.position, rayOrigin.TransformDirection(Vector3.forward) * 1000, Color.white);
+                Debug.Log("no hit");
+            }
+    }
+}
